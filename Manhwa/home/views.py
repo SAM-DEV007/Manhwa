7748bad5cf106fd8b7c4c176928371ad8a69df61
SAM <8dmasters@gmail.com>
2024-10-14 16:29:12 +0530

    Configure views and urls

--- Manhwa/home/views.py
+++ Manhwa/home/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.template import loader
+from django.conf import settings
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
