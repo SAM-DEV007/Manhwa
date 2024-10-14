745ba230dce4ab444b90fd7d0b87bd188d44779c
SAM <8dmasters@gmail.com>
2024-10-14 19:15:49 +0530

    Added template rendering

--- Manhwa/home/views.py
+++ Manhwa/home/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from django.conf import settings
 
 def home(request):
-    return render(request, 'home.html')
+    template = loader.get_template('home.html')
+    return HttpResponse(template.render({'file_location': settings.AWS_S3_CUSTOM_DOMAIN}, request))
