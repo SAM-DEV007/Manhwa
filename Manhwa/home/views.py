39e097efbbfbf50e853ffbda1a783ad3272ed3ae
SAM <8dmasters@gmail.com>
2024-10-14 21:19:21 +0530

    Added db support to load json

--- Manhwa/home/views.py
+++ Manhwa/home/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.conf import settings
+from .models import Manhwa
+
+import json
 
 def home(request):
+    manhwa = Manhwa.objects.values_list('data', flat=True).get(pk=1)
+    manhwa = json.dumps(manhwa)
+
     template = loader.get_template('home.html')
-    return HttpResponse(template.render({'file_location': settings.AWS_S3_CUSTOM_DOMAIN}, request))
+    return HttpResponse(template.render({'file_location': settings.AWS_S3_CUSTOM_DOMAIN, 'json_data': manhwa}, request))
