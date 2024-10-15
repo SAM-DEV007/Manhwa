968ee42e79aa9fcc06e5019cbc163e11d0e5f909
SAM <8dmasters@gmail.com>
2024-10-15 23:03:08 +0530

    Fixed multiple RDS calls

--- Manhwa/home/views.py
+++ Manhwa/home/views.py
@@ -5,9 +5,9 @@ from .models import Manhwa
 
 import json
 
-def home(request):
-    manhwa = Manhwa.objects.values_list('data', flat=True).get(pk=1)
-    manhwa = json.dumps(manhwa)
+manhwa = Manhwa.objects.values_list('data', flat=True).get(pk=1)
+manhwa = json.dumps(manhwa)
 
+def home(request):
     template = loader.get_template('home.html')
     return HttpResponse(template.render({'file_location': settings.AWS_S3_CUSTOM_DOMAIN, 'json_data': manhwa}, request))
