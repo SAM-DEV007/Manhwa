16ec3aa8b20aeed9e7d44d161298c25efe5a77f6
SAM <8dmasters@gmail.com>
2024-10-21 19:33:43 +0530

    Update nginx.sh

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -7,6 +7,7 @@ PROJECT_MAIN_DIR_NAME="Manhwa"
 sudo cp "/home/ubuntu/$PROJECT_MAIN_DIR_NAME/ngnix/$PROJECT_MAIN_DIR_NAME" "/etc/nginx/sites-available/$PROJECT_MAIN_DIR_NAME"
 sudo ln -s /etc/nginx/sites-available/$PROJECT_MAIN_DIR_NAME /etc/nginx/sites-enabled/
 
+sudo unlink /etc/nginx/sites-enabled/default
 sudo gpasswd -a www-data ubuntu
 
 sudo systemctl restart nginx
